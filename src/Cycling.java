06f79951e8395d52047e35654537b72a0b57dd69
Stephen_Laptop <Stephen_Laptop@LAPTOP-GRRJUT4M>
2017-09-09 21:21:03 +1000

    Addition -Random Number Generator to cycling, running, swimming.

--- src/Cycling.java
+++ src/Cycling.java
@@ -1,8 +1,18 @@
 /*
  * Cycling behaviour component
  */
+
+import java.util.concurrent.ThreadLocalRandom;
+
+
 public class Cycling {
+	
+	static private int MINIMUM = 500;
+	static private int MAXIMUM = 800;
+	
 	public void Cycle() {
-		System.out.println("I Cycle");
+		double randomNum = ThreadLocalRandom.current().nextDouble(MINIMUM, MAXIMUM + 1);
+		System.out.printf("I Cycled : %.3f%n", randomNum);
+		
 	}
 }
--- src/Swimming.java
+++ src/Swimming.java
@@ -1,10 +1,19 @@
 /*
  * Swimming behaviour component
  */
-public class Swimming {
+import java.util.concurrent.ThreadLocalRandom;
 
+
+
+public class Swimming {
+	
+	static private int MINIMUM = 100;
+	static private int MAXIMUM = 200;
+	
 	//TODO component return output such as time
 	public void Swim() {
-		System.out.println("I swim");
+		double randomNum = ThreadLocalRandom.current().nextDouble(MINIMUM, MAXIMUM + 1);
+		System.out.printf("I swam : %.3f%n", randomNum);
+		
 	}
 }
